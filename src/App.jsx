be1160dbc0d1ca756ed39ee5c3d945f3db1882fa
ife-codes <ife4373@gmail.com>
2026-06-18 21:10:22 +0100

    Resolve casing issues and added vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import heroImg from "./assets/hero.png";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
-import HowItWorks from "./Components/Howitworks";
+import HowItWorks from "./Components/HowItWorks";
 import WhyPanhive from "./Components/WhyPanhive";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
